41627cfbc9c7621e86de67f18b30ad12b612443a
hivemind02 <hivemind02@gmail.com>
2021-11-29 21:06:46 +0900

    Update render_script.js

--- render_script.js
+++ render_script.js
@@ -1,3 +1,72 @@
-function render() { 
-  return "{}";
+let json_a = {
+    renderList: [
+        {
+            type: "column",
+            children: [
+                {
+                    type: "text",
+                    text: "Hello"
+                },
+                {
+                    type: "row",
+                    children: [
+                        {
+                            type: "text",
+                            text: "<font color='blue'>Jetpack</font> Compose!"
+                        }
+                    ]
+                },
+                {
+                    type: "button",
+                    text: "Button",
+                    action: "click"
+                }
+            ]
+        }
+    ]
+}
+
+let json_b = {
+    renderList: [
+        {
+            type: "column",
+            children: [
+                {
+                    type: "text",
+                    text: "안녕"
+                },
+                {
+                    type: "row",
+                    children: [
+                        {
+                            type: "text",
+                            text: "<font color='red'>제트팩</font> 컴포즈!"
+                        }
+                    ]
+                },
+                {
+                    type: "button",
+                    text: "Button",
+                    action: "default"
+                }
+            ]
+        }
+    ]
+}
+
+var state = 'state_a'
+function render() {
+    let json
+    if (state === 'state_a')
+        json = json_a
+    else
+        json = json_b
+    return JSON.stringify(json)
+}
+function action(event) {
+    if (event === 'click')
+        state = 'state_b'
+    else
+        state = 'state_a'
+    return render()
 }
