06a63c1bfd53f739740f5bcde68ee271e7045013
hivemind02 <hivemind02@gmail.com>
2021-11-29 20:48:41 +0900

    Update render_script.js

--- render_script.js
+++ render_script.js
@@ -1,22 +1,3 @@
-let json_a = {
-  };
-
-let json_b = {
-  };
-    
-    var state = 'state_a';
-    function render() {
-        let json;
-        if (state === 'state_a')
-            json = json_a;
-        else
-            json = json_b;
-        return JSON.stringify(json);
-    }
-    function action(event) {
-        if (event === 'click') 
-            state = 'state_b';
-        else
-            state = 'state_a';
-        return render();
-    }
+function render() { 
+        return JSON.stringify("{}");
+}
